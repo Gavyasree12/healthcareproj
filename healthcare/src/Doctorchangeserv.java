07d246d0d791dc074edfeba0eb5f4d28d266c777
GAVYA SREE U <49972026+Gavyasree12@users.noreply.github.com>
2023-09-11 20:54:00 +0530

    changes done

--- healthcare/src/Doctorchangeserv.java
+++ healthcare/src/Doctorchangeserv.java
@@ -14,32 +14,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Doctorchangeserv
- */
+
 @WebServlet("/Doctorchangeserv")
 public class Doctorchangeserv extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public Doctorchangeserv() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//doGet(request, response);
--- healthcare/src/Doctorserv.java
+++ healthcare/src/Doctorserv.java
@@ -13,9 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Doctorserv
- */
+
 @WebServlet("/Doctorserv")
 public class Doctorserv extends HttpServlet {
 	private static final long serialVersionUID = 1L;
